bbcff0c4d964ce25af5a693edebb05fbc32418dc
pavan2659 <104996648+pavan2659@users.noreply.github.com>
2022-05-07 20:53:20 +0530

    Add files via upload

---
+++ day_03.py
@@ -0,0 +1,19 @@
+""" to print hello world"""
+print('hello world')
+print("hello world")
+print('''hello world''')
+print("""hello world""")
+
+'''to print hello wor'ld'''
+print("hello wor'ld")
+print('''hello wor'ld''')
+print("""hello wor'ld""")
+ 
+'''to print pavan
+kumar'''
+print('''pavan
+ kumar''')
+print("""pavan
+ kumar""")
+print("pavan\nkumar")
+# is used for commenting along with ''''''
